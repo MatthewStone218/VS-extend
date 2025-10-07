9a9b938f0d7b910389c516d2f43c1d54558880d0
Matthew Stone <matthewstone218@gmail.com>
2025-10-07 15:33:30 +0900

    extend

---
+++ VS-extend/ExceptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Threading;
+
+namespace VS_extend
+{
+    public class ExceptionManager
+    {
+        private JoinableTaskFactory _jtf;
+        public CancellationTokenSource CTS;
+        public CancellationToken CT;
+        public List<Task> TaskList;
+        public ExceptionManager(JoinableTaskFactory jtf)
+        {
+            _jtf = jtf;
+            CTS = new CancellationTokenSource();
+            CT = CTS.Token;
+            TaskList = new List<Task>();
+            StartObserving();
+        }
+        private void StartObserving()
+        {
+            JoinableTask jt = _jtf.RunAsync(async () => await WaitAndCancelAllTasksAsync());
+        }
+        private async Task WaitAndCancelAllTasksAsync()
+        {
+            while (true)
+            {
+                Task finishedTask = await Task.WhenAny(TaskList);
+                if (finishedTask.IsFaulted)
+                {
+                    CTS.Cancel();
+                    CTS.Dispose();
+                    CTS = new CancellationTokenSource();
+                    CT = CTS.Token;
+                    TaskList.Clear();
+                }
+            }
+        }
+    }
+}
